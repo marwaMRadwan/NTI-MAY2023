3bb7f55b83ab5d8a66012ec2ba0119c04479e8e3
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-05-20 12:41:37 +0300

    session1

---
+++ session1/custom.js
@@ -0,0 +1,115 @@
+// console.log("hello from js")
+// console.log(5+3)
+// console.log(9*50)
+
+/*
+loose data type
+data type 
+string    "5", 'hello'
+number ( int, float, double, bigInt )  5 , 3 , 5+3, 9*4
+boolean false, true
+null
+undefined
+symbol
+
+int x = 5
+*/
+
+// var x = "hello"
+// w="ay 7aga"
+
+// let y = "hi"
+// const PI = 3.14
+
+// var x = "marwa"
+// y = 3
+// x=4
+
+// console.log(x)
+
+// let x = 10
+
+// let y = 5
+// y = "hello"
+// y = true
+// y = 5.5
+// y = 8*3
+// y = null
+// y = undefined
+// y = "helloz"
+
+//Operators
+//mathmatical (Arithmetic) Operators
+// + - * / % ++ --
+// let val1 = 5, val2 =10, result
+
+// val1 = Number(prompt("please enter x value"))
+// val2 = Number(prompt("please enter y value"))
+// val1 = parseInt(prompt("please enter x value"))
+// val2 = parseInt(prompt("please enter y value"))
+// val1 = +prompt("please enter x value")
+// val2 = +prompt("please enter y value")
+
+// result = val1 + val2
+// document.writeln(result)
+
+// result = val1 - val2
+// document.writeln(result)
+
+// result = val1 * val2
+// document.writeln(result)
+
+// result = val1 / val2
+// document.writeln(result)
+
+// result = val1 % val2   //Modules
+// document.writeln(result)
+
+// console.log(val1++)
+
+// console.log(++val1)
+
+// let x = +prompt("please enter x value") // "5"
+// console.log(x)
+
+//Comparison Operators
+// ==   ===  > < >= <= !=  !==
+// let val1 = "15", val2=15
+// console.log(val1 != val2)
+
+//bitwise Operators
+// | or
+// let x = null
+// let y = x | 5
+
+//Logical Operators
+//  &&   ||  !
+// let userName = "abc", userAge=25, userEndYear=2018, isWorking=false
+// let isValid = userAge<=26 && userEndYear>2016 || isWorking==true
+// console.log(isValid)
+// let a = true
+// !a //=> false
+
+//Assignment Operators
+// =  += -= *= /= 
+
+// let x = 5
+
+// x+=5  // x=x+5   x+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
