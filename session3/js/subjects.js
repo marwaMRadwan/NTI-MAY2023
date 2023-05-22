e7d450cf9c460f3f4df37de3cc5a3910873e0de7
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-05-22 14:21:22 +0300

    s3

--- session3/js/subjects.js
+++ session3/js/subjects.js
@@ -25,10 +25,32 @@ function getCountOfSuccessFail (grades , fullMark ){
     }
 }
 
+function getFinalGrade (failureCount, studentTotal, fullMarks){
+    if( failureCount > 0 ) return "You Got F"
+    let result
+    const studentRate = studentTotal/fullMarks*100
+    if(studentRate>85) result = "A"
+    else if(studentRate>75) result ="B"
+    else if(studentRate>65) result = "C"
+    else if(studentRate>50) result = "D"
+    else result = "F"
+    return result
+}
+
 const subCount = prompt("subject count")
 const subjFullMark = prompt("subject full mark")
 const myGrades = getGradesFromUser(subCount)
 const sumOfGrades = getTotalGrades(myGrades)
 const successAndFailCount = getCountOfSuccessFail(myGrades, subjFullMark)
-console.log(successAndFailCount)
+
+const result = getFinalGrade(
+    successAndFailCount.failCount,
+    sumOfGrades,
+    subCount*subjFullMark
+    )
+document.write(result)
+
+
+
+
 
