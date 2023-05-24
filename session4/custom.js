c444fcf6e28cd202d9c223f939113089a348536a
marwaMRadwan <44086877+marwaMRadwan@users.noreply.github.com>
2023-05-24 12:21:13 +0300

    s4

---
+++ session4/custom.js
@@ -0,0 +1,101 @@
+// DOM => Document Object
+// let data = document.getElementsByTagName("div")
+// console.log(data) // array of elements
+// data = document.getElementsByClassName("row")
+// console.log(data);
+// data = document.getElementById("data")
+// console.log(data);
+// data = document.getElementsByName("data")
+// console.log(data);
+
+// let query = document.querySelector("div")
+// console.log(query)
+// query = document.querySelectorAll("div")
+// console.log(query)
+
+// const data = document.querySelector("#data")
+// data.innerText = "hello from js"
+// console.log(data.innerText)
+// data.innerHTML = "<h3>hello</h3>"
+// console.log(data.textContent)
+
+// const data = document.querySelector("#data")
+// const btn = document.querySelector("#chngMe")
+// btn.addEventListener("click", function(){
+//     data.innerText = "hello from js"
+// })
+
+// function changeTxt(){
+//     data.innerHTML="hello"
+// }
+
+/*
+click => onclick
+mouseover => onmouseover
+mouseout => onmouseout
+mousedown => onmousedown
+mouseup => onmouseup
+===============================
+keyup => onkeyup
+keydown => onkeydown
+**********************************
+
+*/
+
+
+/*
+div => hello
+btn => say hi
+
+div => hi
+btn => say hello
+*/
+
+// const swapper = document.querySelector("#swapper")
+// const data = document.querySelector("#data")
+// swapper.addEventListener("click", function(e){
+//     // console.log(this)
+//     if(this.innerText == "Say Hi"){
+//         this.innerText = "Say Hello"
+//         data.innerText = "Hi"
+//     }
+//     else{
+//         this.innerText = "Say Hi"
+//         data.innerText = "Hello"
+//     }
+// })
+
+// swapper.addEventListener("mouseover", function(){
+//     console.log("test hover")
+//     this.innerText = "Hi"
+//     data.style.backgroundColor = "blue"
+//     // alert("hello ad")
+// })
+// swapper.addEventListener("mouseout", function(){
+//     console.log("test hover")
+//     this.innerText = "Hello"
+// })
+
+
+// const swapper = document.querySelector("#swapper")
+// const data = document.querySelector("#data")
+
+// console.log(swapper.classList)
+
+// swapper.classList = "col-5 bg-light text-dark"
+
+// console.log(swapper.className);
+
+// swapper.classList.add("bg-danger")
+
+// swapper.classList.remove("btn")
+
+
+
+
+
+
+
+
+
+
